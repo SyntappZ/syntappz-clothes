474f5fe57813d04f07b190cfdcec2c037c6d3684
biscuitmanz <doddsanater87@gmail.com>
2018-11-17 21:43:53 +0000

    took out the title fade

--- main.js
+++ main.js
@@ -9,14 +9,12 @@ window.addEventListener('load', () => {
 
 let i = 0;
 let images = [];
-let imageTitles = [];
-let time = 5000;
+let time = 4000;
 
-let = title1 = document.getElementById('tit1');
-let = title2 = document.getElementById('tit2');
-let = title3 = document.getElementById('tit3');
-let = title4 = document.getElementById('tit4');
-let = title5 = document.getElementById('tit5');
+
+
+
+
 
 images[0] = '/img/slider/multi.jpg';
 images[1] = '/img/slider/casual.jpg';
@@ -25,57 +23,47 @@ images[3] = '/img/slider/sportswear.jpg';
 images[4] = '/img/slider/dress.jpg';
 
 
+let = titles = document.getElementsByClassName('tit');
 
 
 
 
+console.log(titles[3])
 
 function changeImg(){
-   imageTitles[i];
+   
    document.slide.src = images[i];
+   
+   
 
     if(i < images.length - 1){
         i++;
     }else{
         i = 0;
     }
-    
-    if(i === 1){
-        title5.style.display = 'none';
-        title4.style.display = 'block';
-        title1.style.opacity = '1';
-        
-    }else{
-        title1.style.opacity = '0';
+   
+    if(images[i] === images[1]){
+        titles[0].style.display = 'block';
+        titles[4].style.display = 'none';
     }
-    if(i === 2){
-        title5.style.display = 'block';
-        title1.style.display = 'none';
-        title2.style.opacity = '1';
-    }else{
-        title2.style.opacity = '0';
+    if(images[i] === images[2]){
+        titles[1].style.display = 'block';
+        titles[0].style.display = 'none';
     }
-    if(i === 3){
-        title2.style.display = 'none';
-        title1.style.display = 'block';
-        title3.style.opacity = '1';
-    }else{
-        title3.style.opacity = '0';
+    if(images[i] === images[3]){
+        titles[2].style.display = 'block';
+        titles[1].style.display = 'none';
     }
-    if(i === 4){
-        title3.style.display = 'none';
-        title2.style.display = 'block';
-        title4.style.opacity = '1';
-    }else{
-        title4.style.opacity = '0';
+    if(images[i] === images[4]){
+        titles[3].style.display = 'block';
+        titles[2].style.display = 'none';
     }
-    if(i === 0){
-        title4.style.display = 'none';
-        title3.style.display = 'block';
-        title5.style.opacity = '1';
-    }else{
-        title5.style.opacity = '0';
+    if(images[i] === images[0]){
+        titles[4].style.display = 'block';
+        titles[3].style.display = 'none';
     }
+
+
 
     
     setTimeout('changeImg()', time);
